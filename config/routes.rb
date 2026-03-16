420919268c94eafca30454c7315712e847fe08dd
elviabth <ejbenedith@gmail.com>
2026-03-16 16:01:46 +0100

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-require "sidekiq/web"
-
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development? || Rails.ENV["STAGING"].present?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   mount Decidim::Core::Engine => "/"
-
-  authenticate :user, ->(user) { user.admin } do
-    mount Sidekiq::Web => "/admin/sidekiq"
-  end
 end
